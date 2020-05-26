872b35f051ae9b8fa5773acd42ecff2823bb4d6a
Computadora <computadora@MacBook-Pro-de-Computadora.local>
2020-05-26 19:19:42 -0300

    Updates

---
+++ precache-manifest.f821e85512545b20984c6c1ee0cbe7d0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e87991a13ade69fb994a8c25668ab259",
+    "url": "/covid-app/index.html"
+  },
+  {
+    "revision": "0d741dc6b83a18898d59",
+    "url": "/covid-app/static/css/main.4926d2ae.chunk.css"
+  },
+  {
+    "revision": "96ca4068c22a710df83e",
+    "url": "/covid-app/static/js/2.e1bf0476.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-app/static/js/2.e1bf0476.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d741dc6b83a18898d59",
+    "url": "/covid-app/static/js/main.4288ac98.chunk.js"
+  },
+  {
+    "revision": "e15ae15f7b48051fefee",
+    "url": "/covid-app/static/js/runtime-main.92748a2f.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid-app/static/media/covid.d7265326.png"
+  }
+]);
